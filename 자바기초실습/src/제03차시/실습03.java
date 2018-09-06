07ceaddea4a8839c7ab4cdb483ff3c402381e30d
user <user@JBTC10110>
2018-09-06 11:22:19 +0900

    제03차시

--- 자바기초실습/src/제03차시/실습03.java
+++ 자바기초실습/src/제03차시/실습03.java
@@ -21,6 +21,29 @@ public class 실습03 {
 		String g2=new String("통신과");
 		System.out.println((f1==f2)+","+(g1==g2));
 		System.out.println(f1.equals(f2)+","+g1.equals(g2));
+		System.out.println(System.identityHashCode(f1)+","+System.identityHashCode(f2)+","+
+		System.identityHashCode(g1)+","+System.identityHashCode(g2));
+		
+		//비트연산자 : 교과서 33쪽
+		System.out.println(0b00000110 & 0b11111101);
+		System.out.println(0b00000110 | 0b00001000);
+		System.out.println(0b00000110 ^ 0b00001111);
+		System.out.println(~0b00000110);
+		System.out.println(0b00010100 >> 2);
+		System.out.println(0b00010100 << 2);
+		
+		System.out.println(0b10000000000000000000000000010100 >> 2);
+		System.out.println(0b10000000000000000000000000010100 >>> 2);
+		
+		System.out.printf("%d, %d", 0b1 << 31, (0b1 << 31)-1);
+		
+		//조건연산자(삼항연산자) : 교과서 34쪽
+		a=2; b=3;
+		System.out.println("큰 값 : \n"+ (a>b?a:b));
+		
+		int o,p,q;
+		o=2; p=10; q=15;
+		System.out.println("최대값 : "+(0>p&&a>q?o:(p>o&&o>q?p:q)));
 	}
 
 }
